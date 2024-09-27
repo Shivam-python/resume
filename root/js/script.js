63b322a6b6422cac4100910f9b46c5d76d32283d
shivam-python <raynoldw222@gmail.com>
2024-09-28 01:27:57 +0530

    feat: light & dark themes added

---
+++ root/js/script.js
@@ -0,0 +1,8 @@
+// typing animation
+
+var typed = new Typed(".typing",{
+    strings : ["","Backend Web Developer","Data Analyst","Pythonist"],
+    typeSpeed : 100,
+    BackSpeed : 60,
+    loop : true
+});
